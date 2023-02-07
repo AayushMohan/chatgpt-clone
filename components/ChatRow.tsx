10d8cf8b90480d61378ae8c445f55e15be66d308
AayushMohan <aayushmohan1702@gmail.com>
2023-02-07 23:09:40 +0530

    Added Chat Bubble

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -1,3 +1,4 @@
+import { ChatBubbleLeftIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import React from "react";
 
@@ -6,7 +7,11 @@ type Props = {
 };
 
 const ChatRow = ({ id }: Props) => {
-  return <Link href={`/chat/${id}`}></Link>;
+  return (
+    <Link href={`/chat/${id}`}>
+      <ChatBubbleLeftIcon className="h-5 w-5" />
+    </Link>
+  );
 };
 
 export default ChatRow;
