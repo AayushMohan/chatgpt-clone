2ef0ae3e5731bc12903c55a050062b33e8d6abe9
AayushMohan <aayushmohan1702@gmail.com>
2023-02-07 22:35:56 +0530

    Added New Chat Component

--- firebase.ts
+++ firebase.ts
@@ -1,4 +1,4 @@
-import { getApp, getApps, initializeApp } from "firebase/app"
+import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 // Your web app's Firebase configuration
@@ -9,11 +9,10 @@ const firebaseConfig = {
   storageBucket: "chatgpt-6ee18.appspot.com",
   messagingSenderId: "48917307486",
   appId: "1:48917307486:web:34b12e91b070ed9f2654cf",
-  measurementId: "G-1Z1RJ331L2"
 };
 
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db }
+export { db };
