05ceb93474dc8a1b63c9784f1e68aef392f79213
AayushMohan <aayushmohan1702@gmail.com>
2023-02-07 17:21:36 +0530

    Added Basic Page Style

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const page = () => {
   return (
-    <div>
-      <h1>ChatGPT</h1>
+    <div className="flex flex-col items-center justify-center text-white h-screen px-2">
+      <h1 className="text-5xl font-bold mb-20">ChatGPT</h1>
     </div>
   );
 };
