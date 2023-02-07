59e5a85ac54b5e9396d6b7e8520359dd0d86739d
AayushMohan <aayushmohan1702@gmail.com>
2023-02-07 22:57:21 +0530

    Updated Firebase

--- firebase.ts
+++ firebase.ts
@@ -8,7 +8,8 @@ const firebaseConfig = {
   projectId: "chatgpt-6ee18",
   storageBucket: "chatgpt-6ee18.appspot.com",
   messagingSenderId: "48917307486",
-  appId: "1:48917307486:web:34b12e91b070ed9f2654cf",
+  appId: "1:48917307486:web:644e19744dba4d8c2654cf",
+  measurementId: "G-C2F0ED1D6T"
 };
 
 // Initialize Firebase
