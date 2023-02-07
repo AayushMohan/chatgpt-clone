8afcf52d7cb0d46bd720eb97f558b8659f69bcbe
AayushMohan <aayushmohan1702@gmail.com>
2023-02-07 19:10:00 +0530

    Added Session Provider and LogIn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,35 @@
+import { SessionProvider } from "../components/SessionProvider";
 import SideBar from "../components/SideBar";
 import "../styles/globals.css";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../pages/api/auth/[...nextauth]";
+import Login from "../components/Login";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html>
       <head />
       <body>
-        <div className="flex">
-          <div className="bg-[#202123] mx-w-xs h-screen overflow-y-scroll md:min-w-[20rem]">
-            <SideBar />
-          </div>
+        <SessionProvider session={session}>
+          {!session ? (
+            <Login />
+          ) : (
+            <div className="flex">
+              <div className="bg-[#202123] mx-w-xs h-screen overflow-y-scroll md:min-w-[20rem]">
+                <SideBar />
+              </div>
 
-          {/* ClientProvider - Notification */}
-          <div className="bg-[#343541] flex-1">{children}</div>
-        </div>
+              {/* ClientProvider - Notification */}
+              <div className="bg-[#343541] flex-1">{children}</div>
+            </div>
+          )}
+        </SessionProvider>
       </body>
     </html>
   );
