76171b0fe7c45742f35de7102b0f6a4670f81b06
AayushMohan <aayushmohan1702@gmail.com>
2023-02-09 23:30:28 +0530

    Added ChatRow

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -1,15 +1,34 @@
-import { ChatBubbleLeftIcon } from "@heroicons/react/24/outline";
+import { ChatBubbleLeftIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { collection, orderBy, query } from "firebase/firestore";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
-import React from "react";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../firebase";
 
 type Props = {
   id: string;
 };
 
 const ChatRow = ({ id }: Props) => {
+  const pathname = usePathname();
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [active, setActive] = useState(false);
+
+  const [messages] = useCollection(
+    query(
+      collection(db, "users", session?.user?.email!, "chats", id, "messages"),
+      orderBy("createdAt", "asc")
+    )
+  );
+
   return (
-    <Link href={`/chat/${id}`}>
+    <Link href={`/chat/${id}`} className={`chatRow justify-center`}>
       <ChatBubbleLeftIcon className="h-5 w-5" />
+      <p className="flex-1 hidden md:inline-flex truncate">New Chat</p>
+      <TrashIcon className="h-5 w-5 text-gray-700 hover:text-gray-200" />
     </Link>
   );
 };
