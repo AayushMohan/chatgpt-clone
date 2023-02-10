72e3eb8e66102dd33ec9c02a8e6631b752904548
AayushMohan <aayushmohan1702@gmail.com>
2023-02-10 21:21:40 +0530

    Added Chat and ChatInput

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Chat from "../../../components/Chat";
+import ChatInput from "../../../components/ChatInput";
 
 const ChatPage = () => {
-  return <div>ChatPage</div>;
+  return (
+    <div className="flex flex-col h-screen overflow-hidden">
+      <Chat />
+      <ChatInput />
+    </div>
+  );
 };
 
 export default ChatPage;
