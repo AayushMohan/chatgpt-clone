9fba0f74ec9980a1ef741bfcff599392a216a36e
AayushMohan <aayushmohan1702@gmail.com>
2023-02-10 22:43:21 +0530

    Added ChatInput

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,12 +1,19 @@
+import { type } from "os";
 import React from "react";
 import Chat from "../../../components/Chat";
 import ChatInput from "../../../components/ChatInput";
 
-const ChatPage = () => {
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+const ChatPage = ({ params: { id } }: Props) => {
   return (
     <div className="flex flex-col h-screen overflow-hidden">
-      <Chat />
-      <ChatInput />
+      <Chat chatId={id} />
+      <ChatInput chatId={id} />
     </div>
   );
 };
--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,7 +1,21 @@
+import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
 import React from "react";
+type Props = {
+  chatId: string;
+};
 
-const ChatInput = () => {
-  return <div>ChatInput</div>;
+const ChatInput = ({ chatId }: Props) => {
+  return (
+    <div>
+      <form className="p-5 space-x-5 flex-1">
+        <input type="text" placeholder="Type your message here..." />
+        <button>
+          <PaperAirplaneIcon className="h-4 w-4 -rotate-45" />
+        </button>
+      </form>
+      <div>{/* Modal Selection */}</div>
+    </div>
+  );
 };
 
 export default ChatInput;
