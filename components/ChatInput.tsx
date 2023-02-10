46e452fc7b0bcb8e7a968b557953c4d82e4e09fe
AayushMohan <aayushmohan1702@gmail.com>
2023-02-10 22:48:22 +0530

    Added Input Value

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,18 +1,28 @@
+"use client";
+
 import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
-import React from "react";
+import { useState } from "react";
 type Props = {
   chatId: string;
 };
 
 const ChatInput = ({ chatId }: Props) => {
+  const [prompt, setPrompt] = useState("");
   return (
     <div>
       <form className="p-5 space-x-5 flex-1">
-        <input type="text" placeholder="Type your message here..." />
-        <button>
+        <input
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+          type="text"
+          placeholder="Type your message here..."
+        />
+
+        <button type="submit" className="">
           <PaperAirplaneIcon className="h-4 w-4 -rotate-45" />
         </button>
       </form>
+
       <div>{/* Modal Selection */}</div>
     </div>
   );
