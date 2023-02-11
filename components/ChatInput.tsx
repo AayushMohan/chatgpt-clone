741f8a4ebc460ea4e708d4ca8ddf409a0395331e
AayushMohan <aayushmohan1702@gmail.com>
2023-02-11 19:04:01 +0530

    Added Session in Buttons

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import { useSession } from "next-auth/react";
 import { useState } from "react";
 type Props = {
   chatId: string;
@@ -8,17 +9,20 @@ type Props = {
 
 const ChatInput = ({ chatId }: Props) => {
   const [prompt, setPrompt] = useState("");
+  const { data: session } = useSession();
   return (
-    <div>
-      <form className="p-5 space-x-5 flex-1">
+    <div className="bg-gray-700/50 text-gray-400 rounded-lg text-sm">
+      <form className="p-5 space-x-5 flex">
         <input
+          className="bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300"
+          disabled={!session}
           value={prompt}
           onChange={(e) => setPrompt(e.target.value)}
           type="text"
           placeholder="Type your message here..."
         />
 
-        <button type="submit" className="">
+        <button type="submit" className="" disabled={!prompt || !session}>
           <PaperAirplaneIcon className="h-4 w-4 -rotate-45" />
         </button>
       </form>
